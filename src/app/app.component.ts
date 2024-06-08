f5fcc3b0706eca251a4bec462e8be4ba21ca2ea5
Idan Sharvit <idan.sharvit@passportcard.co.il>
2024-06-08 16:06:10 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-tlv-conf';
+  @ViewChild('hamburger') hamburger!: ElementRef;
+  @ViewChild('menu') menu!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.hamburger.nativeElement.addEventListener('click', () => {
+      const menu = this.menu.nativeElement;
+      if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+      } else {
+        menu.classList.add('show');
+      }
+    });
+  }
 }
