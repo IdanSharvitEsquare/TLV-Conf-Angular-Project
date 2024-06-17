4188dee970ae991855efa5c524b11e20e15ffffc
Idan Sharvit <idan.sharvit@passportcard.co.il>
2024-06-17 20:02:07 +0300

    added stuff

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,16 +1,12 @@
 import {
   Component,
   OnInit,
-  PLATFORM_ID,
-  TransferState,
-  afterNextRender,
   inject,
-  makeStateKey,
 } from '@angular/core';
 import { Book } from '../../interfaces/book.interface';
 import { BookService } from '../../services/book.service';
 import { Observable } from 'rxjs';
-import { AsyncPipe, isPlatformServer } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-books',
@@ -22,35 +18,12 @@ import { AsyncPipe, isPlatformServer } from '@angular/common';
 export class BooksComponent implements OnInit {
   bookService = inject(BookService);
   books$: Observable<Book[]> | undefined;
-  platformId = inject(PLATFORM_ID);
-  transferState = inject(TransferState);
-  books: Book[] = [];
-
-  // constructor() {
-  //   afterNextRender(() => {
-  //     console.log(localStorage.getItem('token'));
-  //   });
-  // }
 
   ngOnInit(): void {
-    // if (this.transferState.hasKey<Book[]>(makeStateKey<Book[]>('books'))) {
-    //   this.books = this.transferState.get<Book[]>(
-    //     makeStateKey<Book[]>('books'),
-    //     []
-    //   );
-    // } else {
     this.fetchAllBooks();
-    // }
   }
 
   private fetchAllBooks(): void {
     this.books$ = this.bookService.getBooks();
-
-    // this.bookService.getBooks().subscribe((books: Book[]) => {
-    //   if (isPlatformServer(this.platformId)) {
-    //     this.transferState.set<Book[]>(makeStateKey<Book[]>('books'), books);
-    //   }
-    //   this.books = books;
-    // });
   }
 }
