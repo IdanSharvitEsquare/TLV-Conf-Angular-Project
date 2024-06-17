da35a899fb0ee21cf3da7183ca7c61f56e320c30
Idan Sharvit <idan.sharvit@passportcard.co.il>
2024-06-17 19:57:15 +0300

    added stuff

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -9,48 +9,44 @@ import {
 } from '@angular/core';
 import { Book } from '../../interfaces/book.interface';
 import { BookService } from '../../services/book.service';
-import { Observable } from 'rxjs';
-import { AsyncPipe, isPlatformServer } from '@angular/common';
+import { isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss',
 })
 export class BooksComponent implements OnInit {
   bookService = inject(BookService);
-  books$: Observable<Book[]> | undefined;
   platformId = inject(PLATFORM_ID);
   transferState = inject(TransferState);
   books: Book[] = [];
 
-  // constructor() {
-  //   afterNextRender(() => {
-  //     console.log(localStorage.getItem('token'));
-  //   });
-  // }
+  constructor() {
+    afterNextRender(() => {
+      console.log(localStorage.getItem('token'));
+    });
+  }
 
   ngOnInit(): void {
-    // if (this.transferState.hasKey<Book[]>(makeStateKey<Book[]>('books'))) {
-    //   this.books = this.transferState.get<Book[]>(
-    //     makeStateKey<Book[]>('books'),
-    //     []
-    //   );
-    // } else {
-    this.fetchAllBooks();
-    // }
+    if (this.transferState.hasKey<Book[]>(makeStateKey<Book[]>('books'))) {
+      this.books = this.transferState.get<Book[]>(
+        makeStateKey<Book[]>('books'),
+        []
+      );
+    } else {
+      this.fetchAllBooks();
+    }
   }
 
   private fetchAllBooks(): void {
-    this.books$ = this.bookService.getBooks();
-
-    // this.bookService.getBooks().subscribe((books: Book[]) => {
-    //   if (isPlatformServer(this.platformId)) {
-    //     this.transferState.set<Book[]>(makeStateKey<Book[]>('books'), books);
-    //   }
-    //   this.books = books;
-    // });
+    this.bookService.getBooks().subscribe((books: Book[]) => {
+      if (isPlatformServer(this.platformId)) {
+        this.transferState.set<Book[]>(makeStateKey<Book[]>('books'), books);
+      }
+      this.books = books;
+    });
   }
 }
