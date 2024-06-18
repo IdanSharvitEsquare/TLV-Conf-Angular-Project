5663aff73ef93f3f226b70b474f2066635dbc410
IdanSharvitEsquare <idansharvit@e-square.io>
2024-06-19 00:10:57 +0300

    added stuff

--- src/app/components/broken/broken.component.ts
+++ src/app/components/broken/broken.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, isPlatformServer } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  PLATFORM_ID,
+  inject,
+} from '@angular/core';
 
 @Component({
   selector: 'app-broken',
   standalone: true,
   imports: [],
   templateUrl: './broken.component.html',
-  styleUrl: './broken.component.scss'
+  styleUrl: './broken.component.scss',
 })
-export class BrokenComponent {
+export class BrokenComponent implements OnInit {
+  private doc = inject(DOCUMENT);
+  private isServer = isPlatformServer(inject(PLATFORM_ID));
 
+  ngOnInit(): void {
+    if (this.isServer) {
+      const div = this.doc.querySelector('div');
+      div!.remove();
+    }
+  }
 }
